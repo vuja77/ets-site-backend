616703c066cb8f8e485bc755146a4ce23c39a5e3
Vuja <djordjijevujovic210306@gmail.com>
2023-12-31 17:15:51 +0100

    coruse seed thumbnail add

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -14,9 +14,9 @@ class CourseSeeder extends Seeder
     public function run(): void
     {
         DB::table('courses')->insert([
-            ['id' => 1, 'name' => 'Working for home', "course_type_id" => 2],
-            ['id' => 2, 'name' => 'Vibrant orange', "course_type_id" => 2],
-            ['id' => 3, 'name' => 'Leading Remote Teams ', "course_type_id" => 2],
+            ['id' => 1, 'name' => 'Working for home', "course_type_id" => 2, "thumbnail"=> "workingforhome.png"],
+            ['id' => 2, 'name' => 'Vibrant orange', "course_type_id" => 2, "thumbnail"=> "vibranteorange.png"],
+            ['id' => 3, 'name' => 'Leading Remote Teams ', "course_type_id" => 2, "thumbnail"=> "leading.png"],
             ['id' => 4, 'name' => 'New course', "course_type_id" => 1],
             ['id' => 5, 'name' => 'Test course', "course_type_id" => 2],
          
