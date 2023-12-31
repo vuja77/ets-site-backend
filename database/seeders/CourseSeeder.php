e88e1df44515155f0e14d5f548347a6cbb9ba33a
Vuja <djordjijevujovic210306@gmail.com>
2023-12-31 17:17:52 +0100

    coruse seed thumbnail fix

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -17,8 +17,8 @@ class CourseSeeder extends Seeder
             ['id' => 1, 'name' => 'Working for home', "course_type_id" => 2, "thumbnail"=> "workingforhome.png"],
             ['id' => 2, 'name' => 'Vibrant orange', "course_type_id" => 2, "thumbnail"=> "vibranteorange.png"],
             ['id' => 3, 'name' => 'Leading Remote Teams ', "course_type_id" => 2, "thumbnail"=> "leading.png"],
-            ['id' => 4, 'name' => 'New course', "course_type_id" => 1],
-            ['id' => 5, 'name' => 'Test course', "course_type_id" => 2],
+            ['id' => 4, 'name' => 'New course', "course_type_id" => 1, "thumbnail"=> "leading.png"],
+            ['id' => 5, 'name' => 'Test course', "course_type_id" => 2, "thumbnail"=> "leading.png"],
          
         ]);
     }
